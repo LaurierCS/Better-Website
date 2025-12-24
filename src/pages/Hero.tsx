f8d8a9065c4f8579025e808d680c0533869001b9
Flapjacck <plkelly333@me.com>
2025-12-24 17:24:32 -0500

    Fix: update imports

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -2,13 +2,15 @@
  * Hero page
  */
 
-import { Welcome } from '../components/hero/welcome';
+import { Welcome } from '../components/hero/Welcome';
 
 export const HeroPage = () => {
   return (
-    <section className="w-full min-h-screen flex items-center justify-center bg-linear-to-b from-slate-800 to-slate-900">
-      <Welcome />
-    </section>
+    <>
+      <section className="w-full min-h-screen flex items-center justify-center bg-linear-to-b from-slate-800 to-slate-900">
+        <Welcome />
+      </section>
+    </>
   );
 };
 
