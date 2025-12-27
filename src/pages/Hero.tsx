10b97e758530ef2b04084b448d5ba0e77d70c38d
Flapjacck <plkelly333@me.com>
2025-12-27 12:49:12 -0500

    Fix: stats on hero

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -3,12 +3,14 @@
  */
 
 import { Welcome } from '../components/hero/Welcome';
+import { Stats } from '../components/hero/Stats';
 
 export const HeroPage = () => {
   return (
     <>
-      <section className="w-full min-h-screen flex items-center justify-center bg-linear-to-b from-slate-800 to-slate-900">
+      <section className="w-full min-h-screen flex flex-col items-center justify-center bg-linear-to-b from-slate-800 to-slate-900">
         <Welcome />
+        <Stats />
       </section>
     </>
   );
