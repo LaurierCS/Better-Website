e6111753d52910544d4aa1dc6ca1fdf939f8c6fa
Flapjacck <plkelly333@me.com>
2025-12-26 21:05:53 -0500

    Fix: teampage on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/universal/Header';
 import HeroPage from './pages/Hero';
+import { TeamSection } from './pages/Team';
 
 function App() {
   return (
     <>
       <Header />
       <HeroPage />
+      <TeamSection />
     </>
   );
 }
