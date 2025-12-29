6f34e8853f69952f56d2a3261868e3c729626135
Flapjacck <plkelly333@me.com>
2025-12-29 16:08:33 -0500

    Fix: HtmlBox on Hero

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -4,12 +4,14 @@
 
 import { Welcome } from '../components/hero/Welcome';
 import { Stats } from '../components/hero/Stats';
+import HTMLBox from '../components/hero/HTMLBox';
 
 export const HeroPage = () => {
   return (
     <>
       <section className="w-full min-h-screen flex flex-col items-center justify-center bg-linear-to-b from-slate-800 to-slate-900">
         <Welcome />
+        <HTMLBox />
         <Stats />
       </section>
     </>
