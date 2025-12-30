38920c75ba8d8d5cbcb9966bd48d81475727d08d
Flapjacck <plkelly333@me.com>
2025-12-30 15:21:19 -0500

    Feat: simple Initiatives page

---
+++ src/pages/Initiatives.tsx
@@ -0,0 +1,9 @@
+import SpeakerSignup from '../components/initiatives/SpeakerSignup';
+
+export default function Initiatives() {
+  return (
+    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-linear-to-b from-slate-800 to-slate-900">
+        <SpeakerSignup />
+    </div>
+  );
+}
