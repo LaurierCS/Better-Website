169c75e83022b41ffb3cdd033435db5084c1e85d
Shazzz <nickshahbaz135@gmail.com>
2026-01-01 20:05:13 -0500

    fixing conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+
 import Header from './components/universal/Header';
 import HeroPage from './pages/Hero';
 import Events from './pages/Events';
+// Initiatives component import (to be created)
+// Remove the below line and uncomment the import when Initiatives is implemented
+// import Initiatives from './pages/Initiatives';
+const Initiatives = () => <div>Initiatives section coming soon.</div>;
 import { TeamSection } from './pages/Team';
 
 function App() {
@@ -9,6 +14,7 @@ function App() {
       <Header />
       <HeroPage />
       <Events />
+      <Initiatives />
       <TeamSection />
     </>
   );
