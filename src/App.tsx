04b757d5e42568e7b6f2a01fd889c491845252a8
Shazzz <nickshahbaz135@gmail.com>
2026-01-03 17:53:21 -0500

    :(((

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@
 import Header from './components/universal/Header';
 import HeroPage from './pages/Hero';
 import Events from './pages/Events';
-// Initiatives component import (to be created)
-// Remove the below line and uncomment the import when Initiatives is implemented
-// import Initiatives from './pages/Initiatives';
-const Initiatives = () => <div>Initiatives section coming soon.</div>;
+import Initiatives from './pages/Initiatives';
 import { TeamSection } from './pages/Team';
 
 function App() {
