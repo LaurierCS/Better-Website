4ef1aab7af82349118753590ea99ea2229a92ba5
Shazzz <nickshahbaz135@gmail.com>
2026-01-10 16:47:46 -0500

    fixed events alignment

--- src/components/events/Info.tsx
+++ src/components/events/Info.tsx
@@ -6,7 +6,7 @@ type InfoProps = {
 
 function Info({ title, description, date }: InfoProps) {
   return (
-    <div className="bg-[#05081a] text-white font-[Dosis] w-full m-0 p-0 rounded-t-2xl">
+    <div className="bg-[#05081a] text-white font-[Dosis] w-full rounded-t-2xl m-0 p-0">
       <div className="flex items-center justify-between px-8 pt-4 pb-1">
         <h2 className="text-2xl font-bold uppercase tracking-wide m-0 p-0">{title}</h2>
         {date && <span className="italic text-base m-0 pr-[10px] p-0">{date}</span>}
