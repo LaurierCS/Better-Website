5267ba0118ad960979c1ccb2b3117ac26c597aea
Flapjacck <plkelly333@me.com>
2026-01-10 11:11:12 -0500

    Fix: removed asset gallery

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Header from './components/universal/Header';
 import HeroPage from './pages/Hero';
 import Events from './pages/Events';
 import Initiatives from './pages/Initiatives';
-import AssetGallery from './pages/AssetGallery';
 import { TeamSection } from './pages/Team';
 
 function App() {
@@ -13,8 +12,6 @@ function App() {
       <Events />
       <Initiatives />
       <TeamSection />
-      {/* TEMP: Asset Gallery Page */}
-      <AssetGallery />
     </>
   );
 }
