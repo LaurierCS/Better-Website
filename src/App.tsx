fd9f1b6e281d937f6772d6baf56ba6bbb4301552
Spencer Kelly <126588898+Flapjacck@users.noreply.github.com>
2026-01-12 11:23:59 -0500

    Revert "redone"
    This reverts commit b8dbb457e56e5a782346cc1fb92afe469f5edf5a.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Header from './components/universal/Header';
 import { PatternBackground } from './components/universal/PatternBackground';
-import { Particles } from './components/universal/Particles';
 import HeroPage from './pages/Hero';
 import Events from './pages/Events';
 import Initiatives from './pages/Initiatives';
@@ -10,7 +9,6 @@ function App() {
   return (
     <div className="relative w-full min-h-screen" style={{ minHeight: '100vh', position: 'relative', overflow: 'auto' }}>
       <PatternBackground />
-      <Particles />
       <div className="relative z-10">
         <Header />
         <HeroPage />
