562c8ff0db36bd0a37a8f3f2dbcd59ceb5641168
Shazzz <nickshahbaz135@gmail.com>
2026-01-13 18:31:16 -0500

    fixed scraptext and started meet the pros component

--- src/pages/Initiatives.tsx
+++ src/pages/Initiatives.tsx
@@ -1,8 +1,10 @@
 import SpeakerSignup from '../components/initiatives/SpeakerSignUp';
+import MeetThePros from '../components/initiatives/MeetThePros';
 
 export default function Initiatives() {
   return (
     <div className="w-full min-h-screen flex flex-col items-center justify-center overflow-hidden px-2 sm:px-4">
+      <MeetThePros />
       <SpeakerSignup />
     </div>
   );
